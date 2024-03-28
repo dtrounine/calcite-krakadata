909ba39f6f7d24df69e3105b136e1bb12eb62564
Dmitry Trunin <dtrounine@gmail.com>
2024-03-28 17:38:33 +0100

    Updated gradle files for publishing to dedicated krakadata repository

--- build.gradle.kts
+++ build.gradle.kts
@@ -133,7 +133,8 @@ tasks.validateBeforeBuildingReleaseArtifacts {
 
 val String.v: String get() = rootProject.extra["$this.version"] as String
 
-val buildVersion = "calcite".v + releaseParams.snapshotSuffix
+//val buildVersion = "calcite".v + releaseParams.snapshotSuffix
+val buildVersion = "calcite".v
 
 println("Building Apache Calcite $buildVersion")
 
